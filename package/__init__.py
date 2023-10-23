3fbb6025027b148aeeedcbc81aadfc083fb1d8d4
LeroyK <785632486@qq.com>
2023-10-23 23:01:39 +0800

    1

---
+++ package/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+# 划定导入的子模块，from sound.effects import * 专用
+__all__ = ['']
+__doc__ = "注释"
+"""
+包初始化的空间使用
+"""
+import json
+
+
+info = "全局常量"
+
+
+def testGlobal():
+    print("testGlobal")
